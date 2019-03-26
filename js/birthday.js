cb98b883dde43afaecf79565702a9e99a06d5186
Rafii Ramii <info.rafyramy@gmail.com>
2019-03-26 23:33:46 +0300

    added click event to the paragraphs

--- js/birthday.js
+++ js/birthday.js
@@ -1,1 +1,12 @@
-${}
+$(document).ready(function(){
+    $("#p2, #p3, #p4").hide();
+    $("#p1").click(function(){
+      $("#p2").show();
+    });
+    $("#p2").click(function(){
+        $("#p3").show();
+      });
+      $("#p3").click(function(){
+        $("#p4").show();
+      });
+  });
